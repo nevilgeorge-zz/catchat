0db63c9d00d022635c670247000c8f5df7baf002
Nevil George <nevilgeorge2016@u.northwestern.edu>
2014-07-08 22:27:55 -0700

    Commit from 7/08

--- app.js
+++ app.js
@@ -27,6 +27,18 @@ io.on('connection', function(socket){
 	});
 
 	socket.on('join', function(name) {
+		redisClient.lrange('users', 0, -1, function(err, reply) {
+			for (var i = 0; i < reply.length; i++) {
+				if (reply[i] == name) {
+					var error = 'Nickname already exists!';
+					console.log(error);
+					socket.emit('user error', error);
+				}
+			}
+		});
+	});
+
+	socket.on('join', function(name) {
 		redisClient.lrange('messages', 0, -1, function(err, reply) {
 			var messages = reply.reverse();
 			reply.forEach(function(message) {
--- clientSide.js
+++ clientSide.js
@@ -2,7 +2,15 @@ var socket = io();
 
 socket.on('connect', function() {
 	var nickname = prompt('What is your nickname?');
+	if (nickname === null) {
+		nickname = 'Untamed Wildcat';
+	}
 	socket.emit('join', nickname);
+});
+
+socket.on('user error', function(message) {
+	console.log(message);
+	alert(message);
 });
 
 $('form').submit(function(event) {
